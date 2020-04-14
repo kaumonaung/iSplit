da6c6d2cdb128d044661566f40f1e513f65c9a7f
kaumonaung <ag.kaumon@gmail.com>
2020-04-15 00:37:04 +0200

    Finish layout and model

--- iSplit/ContentView.swift
+++ iSplit/ContentView.swift
@@ -9,11 +9,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    // MARK: - Bindings & Computed Properties
+    @State private var priceAmount = ""
+    @State private var peopleAmount = ""
+    @State private var tipAmount = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    // Computes the total check with tip
+    var totalCheckAmountWithTip: Double {
+        let amount = Double(priceAmount) ?? 0
+        let tipSelection = tipAmount
+        let tipAmount = Double(tipPercentages[tipSelection])
+        
+        let tipInPercentage = tipAmount / 100
+        let totalCheck = amount * (1 + tipInPercentage)
+        
+        return totalCheck
     }
-}
+    
+    // Computes only the tip amount
+    var onlyTip: Double {
+        let amount = Double(priceAmount) ?? 0
+        let tipSelection = tipAmount
+        let tipAmount = Double(tipPercentages[tipSelection])
+        
+        let tipInPercentage = tipAmount / 100
+        let onlyTipAmount = tipInPercentage * amount
+        
+        return onlyTipAmount
+    }
+    
+    // Computes the amount per person
+    var amountPerPerson: Double {
+        let amount = Double(priceAmount) ?? 0
+        let tipSelection = tipAmount
+        let tipAmount = Double(tipPercentages[tipSelection])
+        let amountOfPeople = Double(peopleAmount) ?? 1
+        
+        let tipInPercentage = tipAmount / 100
+        let totalCheck = amount * (1 + tipInPercentage)
+        let perPerson = totalCheck / amountOfPeople
+        
+        return perPerson
+    }
+    
+    // MARK: - Content View
+    var body: some View {
+        
+        NavigationView {
+            
+            Form {
+                Section(header: Text("Check amount")) {
+                    TextField("Enter check amount here ...", text: $priceAmount)
+                }
+                Section(header: Text("Number of people")) {
+                    TextField("Enter number of people here ...", text: $peopleAmount)
+                }
+                Section(header: Text("Select the tip amount")) {
+                    Picker(selection: $tipAmount, label: Text("Select the tip amount")) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    } .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Check amount")) {
+                    Text("Tip: \(onlyTip, specifier: "%.2f")$")
+                }
+                Text("Total: \(totalCheckAmountWithTip, specifier: "%.2f")$")
+                    .font(.headline)
+                Text("Per Person: \(amountPerPerson, specifier: "%.2f")$")
+                    .font(.headline)
+            }
+            .navigationBarTitle("iSplit", displayMode: .large)
+        }
+    } // End body
+} // End ContentView
 
+// MARK: - Preview Provider
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
