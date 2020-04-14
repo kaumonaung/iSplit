c64e0e1b9f9ac7656c7dbb9d2956e9b4f2cfe108
kaumonaung <ag.kaumon@gmail.com>
2020-04-14 21:38:26 +0200

    Initial Commit

---
+++ iSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSplit
+//
+//  Created by Kaumon Aung on 14.04.20.
+//  Copyright © 2020 Kaumon Aung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
